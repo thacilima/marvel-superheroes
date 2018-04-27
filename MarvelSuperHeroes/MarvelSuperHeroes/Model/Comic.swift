a7751d3c67dc680cb1e57435836f0f3561fefe59
Thaciana Lima <thacilima@gmail.com>
2018-04-27 00:47:14 -0300

    Comic model created.

---
+++ MarvelSuperHeroes/MarvelSuperHeroes/Model/Comic.swift
@@ -0,0 +1,22 @@
+//
+//  Comic.swift
+//  MarvelSuperHeroes
+//
+//  Created by Thaciana Lima on 27/04/18.
+//  Copyright © 2018 TL. All rights reserved.
+//
+
+import Foundation
+
+struct Comic: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name = "title"
+        case description
+    }
+    
+    let id: Int
+    let name: String
+    let description: String
+}
